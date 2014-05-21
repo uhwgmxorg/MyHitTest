12c80874fce67903297692a7a9dc1e6aac485ef8
Uwe On WS04 <uhw@gmx.org>
2014-05-21 21:14:46 +0200

    Start.

---
+++ MyHitTest/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿/******************************************************************************/
+/*                                                                            */
+/*   Program: MyHitTest                                                       */
+/*   A smal sample for a WPF Hit-Test                                         */
+/*                                                                            */
+/*   21.5.2014 0.0.0.0 uhwgmxorg Start                                        */
+/*                                                                            */
+/******************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyHitTest
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        MyVisualHost _visualHost = new MyVisualHost();
+
+        public double DotSize { get; set; }
+        public string DotColor { get; set; }
+
+        /// <summary>
+        /// Properies
+        /// </summary>
+        private bool checkBoxAdd;
+        public bool CheckBoxAdd { get { return checkBoxAdd; } set { checkBoxAdd = value; OnPropertyChanged("CheckBoxAdd"); } }
+        private string message;
+        public string Message { get { return message; } set { message = value; OnPropertyChanged("Message"); } }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        /******************************/
+        /*           Events           */
+        /******************************/
+        #region Events
+
+        /// <summary>
+        /// Window_Loaded
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _visualHost = new MyVisualHost();
+            _visualHost.Hit += HitHandler;
+            MyCanvas.Children.Add(_visualHost);
+        }
+
+        /// <summary>
+        /// Hit
+        /// </summary>
+        public void HitHandler()
+        {
+            Console.Beep();
+            Message = "Hit !!";
+        }
+
+        /// <summary>
+        /// Window_MouseLeftButtonDown
+        /// add a dot
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!CheckBoxAdd) return;
+            var pos = Mouse.GetPosition(this);
+            DotSize = 10;
+            DotColor = "#FFFF0000";
+            _visualHost.AddDot(pos,DotSize,(SolidColorBrush)(new BrushConverter().ConvertFrom(DotColor)));
+            Message = "Add";
+        }
+
+        /// <summary>
+        /// Window_MouseRightButtonDown
+        /// clear all dots
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _visualHost.Clear();
+            Message = "Clear";
+        }
+
+        #endregion
+        /******************************/
+        /*      Other Functions       */
+        /******************************/
+        #region Other Functions
+
+        /// <summary>
+        /// OnPropertyChanged
+        /// </summary>
+        /// <param name="p"></param>
+        private void OnPropertyChanged(string p)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+        }
+
+        #endregion
+    }
+}
---
+++ MyHitTest/MyVisualHost.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace MyHitTest
+{
+    public class MyVisualHost : FrameworkElement
+    {
+        public delegate void HitEventHandler();
+        public event HitEventHandler Hit;
+
+        private VisualCollection children;
+        public VisualCollection Children {get;set;}
+        protected override int VisualChildrenCount
+        {
+            get { return children.Count; }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MyVisualHost()
+        {
+            children = new VisualCollection(this);
+
+            MouseLeftButtonDown += new MouseButtonEventHandler(MyVisualHost_MouseLeftButtonDown);
+        }
+
+        /******************************/
+        /*           Events           */
+        /******************************/
+        #region Events
+
+        /// <summary>
+        /// GetVisualChild
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        protected override Visual GetVisualChild(int index)
+        {
+            if (index < 0 || index >= children.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return children[index];
+        }
+
+        /// <summary>
+        /// MyVisualHost_MouseLeftButtonUp
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void MyVisualHost_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Hit != null)
+                Hit();
+        }
+
+        #endregion
+        /******************************/
+        /*      Other Functions       */
+        /******************************/
+        #region Other Functions
+
+        public void AddDot(Point p,double size, Brush color)
+        {
+            DrawingVisual drawingVisual = new DrawingVisual();
+            DrawingContext drawingContext = drawingVisual.RenderOpen();
+            drawingContext.DrawEllipse(color, null, p, size, size);
+            drawingContext.Close();
+            children.Add(drawingVisual);
+        }
+
+        /// <summary>
+        /// Clear
+        /// </summary>
+        public void Clear()
+        {
+            children.Clear();
+        }
+
+        #endregion
+    }
+}
